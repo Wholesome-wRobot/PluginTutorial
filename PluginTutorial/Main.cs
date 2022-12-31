b256353baa2287473d65588f859140523f4ea976
Wholesome-wRobot <jeanlouisledaron@gmail.com>
2022-12-31 20:53:02 +0100

    Add project files.

---
+++ PluginTutorial/Main.cs
@@ -0,0 +1,76 @@
+﻿using PluginTutorial;
+using robotManager.Helpful;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using wManager.Plugin;
+using wManager.Wow.Helpers;
+using wManager.Wow.ObjectManager;
+
+public class Main : IPlugin
+{
+    private bool _isRunning;
+    private List<WoWUnit> _attackableUnits = new List<WoWUnit>();
+
+    public void Initialize()
+    {
+        TutorialPluginSettings.Load();
+        Logging.Write("PluginTutorial initialized");
+        _isRunning = true;
+        EventsLuaWithArgs.OnEventsLuaStringWithArgs += PluginEventHandler;
+        if (TutorialPluginSettings.CurrentSettings.EnableRadar)
+        {
+            Radar3D.OnDrawEvent += DrawRadar;
+            Radar3D.Pulse();
+        }
+        PluginLoop();
+    }
+
+    public void Dispose()
+    {
+        Logging.Write("PluginTutorial disposed");
+        EventsLuaWithArgs.OnEventsLuaStringWithArgs -= PluginEventHandler;
+        if (TutorialPluginSettings.CurrentSettings.EnableRadar)
+        {
+            Radar3D.OnDrawEvent -= DrawRadar;
+        }
+        _isRunning = false;
+    }
+
+    private void PluginEventHandler(string eventid, List<string> args)
+    {
+        if (eventid == "AUTOEQUIP_BIND_CONFIRM")
+        {
+            Lua.LuaDoString("StaticPopup1Button1:Click();");
+        }
+    }
+
+    private void DrawRadar()
+    {
+        foreach (WoWUnit unit in _attackableUnits)
+        {
+            Radar3D.DrawCircle(unit.Position, 1.5f, Color.Red);
+            Radar3D.DrawLine(ObjectManager.Me.Position, unit.Position, Color.Red, 100);
+        }
+    }
+
+    private void PluginLoop()
+    {
+        while (_isRunning)
+        {
+            if (Conditions.InGameAndConnectedAndProductStartedNotInPause)
+            {
+                _attackableUnits = ObjectManager.GetWoWUnitAttackables();
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+
+    public void Settings()
+    {
+        TutorialPluginSettings.Load();
+        TutorialPluginSettings.CurrentSettings.ToForm();
+        TutorialPluginSettings.CurrentSettings.Save();
+    }
+}
---
+++ PluginTutorial/TutorialPluginSettings.cs
@@ -0,0 +1,58 @@
+﻿using robotManager.Helpful;
+using System;
+using System.ComponentModel;
+using System.IO;
+using wManager.Wow.Helpers;
+using wManager.Wow.ObjectManager;
+
+namespace PluginTutorial
+{
+    public class TutorialPluginSettings : Settings
+    {
+        private static readonly string _productName = "PluginTutorial";
+        private static string GetSettingsPath => AdviserFilePathAndName(_productName, ObjectManager.Me.Name + "." + Usefuls.RealmName);
+        public static TutorialPluginSettings CurrentSettings { get; set; }
+
+        [DefaultValue(false)]
+        [Category("Settings")]
+        [DisplayName("Enable radar")]
+        [Description("Shows the enemies around you")]
+        public bool EnableRadar { get; set; }
+
+        public TutorialPluginSettings()
+        {
+            EnableRadar = false;
+        }
+
+        public bool Save()
+        {
+            try
+            {
+                return Save(GetSettingsPath);
+            }
+            catch (Exception ex)
+            {
+                Logging.WriteError($"{_productName} > Save(): " + ex);
+                return false;
+            }
+        }
+
+        public static bool Load()
+        {
+            try
+            {
+                if (File.Exists(GetSettingsPath))
+                {
+                    CurrentSettings = Load<TutorialPluginSettings>(GetSettingsPath);
+                    return true;
+                }
+                CurrentSettings = new TutorialPluginSettings();
+            }
+            catch (Exception ex)
+            {
+                Logging.WriteError($"{_productName} > Load(): " + ex);
+            }
+            return false;
+        }
+    }
+}
